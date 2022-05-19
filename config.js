b280f95ebbc34fda8951b11e578b827571623629
ZallXd <zallgemge@gmail.com>
2022-05-20 06:32:25 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,53 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['6285731706147']
+global.premium = ['6285731706147']
+global.ownername = 'Zall Pesstore'
+global.botname = 'Zets Public'
+global.packname = '© ZALL - MULTI DEVICE'
+global.gc = 'https://chat.whatsapp.com/CJp4EUkUOaX3IgD5UA6t2V'
+global.limitawal = '100'
+global.author = '@naando.jpeg\n@alya.xzy'
+global.sessionName = 'sessionya'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⌕'
+global.mess = {
+    success: 'Berhasil',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Tunggu sebentar, sedang di proses',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.sc = fs.readFileSync('./media/sc.jpg')
+global.tq = fs.readFileSync('./media/tq.jpg')
+global.menu = fs.readFileSync('./media/menu.jpg')
+global.allmenu = fs.readFileSync('./media/allmenu.jpg')
+global.thumb = fs.readFileSync('./media/pepe.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
